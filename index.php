d53e2585a0fa64d57673b36ae79f18771a8ed26d
Thomas Dutrion <thomas@engineor.com>
2018-11-27 16:07:59 +0100

    Add zend diactoros implementation

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 
-use InvoiceApp\Http\Request;
+use Zend\Diactoros\ServerRequestFactory;
 
 require __DIR__.'/vendor/autoload.php';
 
+$request = ServerRequestFactory::fromGlobals();
+
 $repository = new \InvoiceApp\Repository\Invoice();
 $action = new \InvoiceApp\Action\Invoice\Listing($repository);
-$response = $action->handle(new Request());
+$response = $action->handle($request);
 
 foreach ($response->getHeaders() as $key => $value) {
+    $value = implode(', ', $value);
     header("$key: $value");
 }
 echo $response->getBody();
--- src/Action/Invoice/Listing.php
+++ src/Action/Invoice/Listing.php
@@ -6,7 +6,7 @@ namespace InvoiceApp\Action\Invoice;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\StreamInterface;
+use Zend\Diactoros\Response;
 
 class Listing
 {
@@ -19,38 +19,12 @@ class Listing
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $repository = $this->repository;
-        $response = new class($request, $repository) implements ResponseInterface {
-            private $request;
-            private $repository;
+        ob_start();
+        $name = $request->getQueryParams()['name'];
+        $invoices = $this->repository->findAll();
+        require __DIR__.'/../../../templates/invoice/listing.php';
+        $content = ob_get_clean();
 
-            public function __construct(ServerRequestInterface $request, \InvoiceApp\Repository\Invoice $repository)
-            {
-                $this->request = $request;
-                $this->repository = $repository;
-            }
-            public function getProtocolVersion() {}
-            public function withProtocolVersion($version) {}
-            public function getHeaders() {}
-            public function hasHeader($name) {}
-            public function getHeader($name) {}
-            public function getHeaderLine($name) {}
-            public function withHeader($name, $value) {}
-            public function withAddedHeader($name, $value) {}
-            public function withoutHeader($name) {}
-            public function getBody() {
-                ob_start();
-                $name = $this->request->getAttribute('name');
-                $invoices = $this->repository->findAll();
-                require __DIR__.'/../../../templates/invoice/listing.php';
-                return ob_get_clean();
-            }
-            public function withBody(StreamInterface $body) {}
-            public function getStatusCode() {}
-            public function withStatus($code, $reasonPhrase = '') {}
-            public function getReasonPhrase() {}
-        };
-
-        return $response;
+        return new Response\HtmlResponse($content);
     }
 }
